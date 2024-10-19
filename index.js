1668640e7d76e12d484d4e8c8e0c2f73102bf226
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-10-19 16:12:46 -0400

    Refactor retrievePic function to handle both user-selected date and today's date

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ import { apiKey } from "./apiKey.js";
 
 const getPicBtn = document.querySelector('#getPic'); 
 getPicBtn.addEventListener('click', retrievePic); 
+getTodayPic.addEventListener('click', retrievePic)
 
-function retrievePic() {
-  const date = document.querySelector('#date').value;
-
-  const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`;
+function retrievePic(e) {
+   
+  let url; 
+  if (e.target.id === 'getPic') {
+    const dateInput = document.querySelector('#date'); 
+    const date = dateInput.value;
+    if (!date) {
+      alert('Enter a date'); 
+      return; 
+    }
+    url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`;
+    document.querySelector('.dateTitle').textContent = date;
+    dateInput.value = ''; 
+    
+  }
+  else {
+    let today = new Date(); 
+    const [month, day, year] = [
+      today.getMonth(),
+      today.getDate(),
+      today.getFullYear(),
+    ];
+    today = [year, month, day].join('-');
+    url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${today}`;
+    document.querySelector('.dateTitle').textContent = today;
+  }
+  
   const title = document.querySelector('.title'); 
   const description = document.querySelector('.description'); 
   const img = document.querySelector('img'); 
@@ -15,19 +39,21 @@ function retrievePic() {
   fetch(url)
     .then((resp) => resp.json())
     .then((result) => {
-      console.log(result); 
       title.innerText = result.title; 
       description.innerText = result.explanation; 
       if (result.media_type === 'image') {
         img.classList.remove('hidden');
         img.src = result.hdurl; 
         iframe.classList.add('hidden'); 
+        iframe.src = '';
       }
       else {
         iframe.classList.remove('hidden');
-        iframe.src = result.hdurl;
+        iframe.src = result.url;
+        iframe.title = result.title; 
         img.classList.add('hidden');
+        img.src = '';
       }
-    });
+    }).catch(err => console.error(err));
 }
 
